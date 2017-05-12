054dd37a5ac9b2a98d3487c0ed78750941fee690
Rebecca Simpson <rs6713@eews304-010.ee.ic.ac.uk>
2017-05-12 22:35:26 +0100

    Created vectors

--- word2vec/trunk/demo-phrases.sh
+++ word2vec/trunk/demo-phrases.sh
@@ -1,9 +1,9 @@
 make
-if [ ! -e news.2012.en.shuffled ]; then
-  wget http://www.statmt.org/wmt14/training-monolingual-news-crawl/news.2012.en.shuffled.gz
-  gzip -d news.2012.en.shuffled.gz -f
-fi
-sed -e "s/’/'/g" -e "s/′/'/g" -e "s/''/ /g" < news.2012.en.shuffled | tr -c "A-Za-z'_ \n" " " > news.2012.en.shuffled-norm0
+#if [ ! -e descriptions.txt ]; then
+ # wget http://www.statmt.org/wmt14/training-monolingual-news-crawl/news.2012.en.shuffled.gz
+#  gzip -d news.2012.en.shuffled.gz -f
+#fi
+sed -e "s/’/'/g" -e "s/′/'/g" -e "s/''/ /g" < descriptions.txt | tr -c "A-Za-z'_ \n" " " > news.2012.en.shuffled-norm0
 time ./word2phrase -train news.2012.en.shuffled-norm0 -output news.2012.en.shuffled-norm0-phrase0 -threshold 200 -debug 2
 time ./word2phrase -train news.2012.en.shuffled-norm0-phrase0 -output news.2012.en.shuffled-norm0-phrase1 -threshold 100 -debug 2
 tr A-Z a-z < news.2012.en.shuffled-norm0-phrase1 > news.2012.en.shuffled-norm1-phrase1
