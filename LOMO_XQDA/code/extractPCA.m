d948ac166dc5431389f763f1805b1966e743e4b5
Rebecca Simpson <rs6713@eews305-029.ee.ic.ac.uk>
2017-05-21 20:38:15 +0100

    Projected images to word vector space

---
+++ LOMO_XQDA/code/extractPCA.m
@@ -0,0 +1,23 @@
+%Input rows are the sentences, cols are the variabls
+function sentenceVectors=extractPCA(sentences, numDims)
+    idx=randperm(size(sentences,2));
+    newSize=5000;
+    
+    meanS=mean(sentences,2);%col vector
+    sentenceVector=bsxfun(@minus,sentences,meanS);
+    covy=sentenceVector(:,idx(1:newSize)).'*sentenceVector(:,idx(1:newSize))/size(sentenceVector,1);
+    [eigVecs, eigVals]=eig(covy);%eigVecs cols are eigenVectors
+    eigVals=diag(eigVals);%col vector
+    eigVals=eigVals.';
+    [eigVals,i]=sort(eigVals,'descend');
+    eigVecs(:,:)=eigVecs(:,i);
+    eigVecs=eigVecs(:,1:numDims);
+    
+    for s= 1: size(sentences,1);
+        for i=1: numDims
+            temp= sentenceVector(s,idx(1:newSize))*eigVecs(:,i);
+            sentenceVectors(s,i)= temp;
+        end
+    end
+    
+end
